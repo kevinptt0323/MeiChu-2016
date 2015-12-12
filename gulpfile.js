c016edd560ca0682e9ec204513d5cf0673244e26
Kevin Lin <kevinptt0323@gmail.com>
2015-12-13 00:55:44 +0800

    setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var
+	gulp        = require('gulp'),
+	less        = require('gulp-less'),
+	changed     = require('gulp-changed'),
+	browserify  = require('gulp-browserify')
+;
+
+var paths = {
+	webpages: {
+		src: ['./src/**/*.html', './src/**/*.php'],
+		dest: './dist'
+	},
+	js: {
+		src: './src/js/**/*.js',
+		dest: './dist/js'
+	},
+	jsx: {
+		src: './src/js/**/*.jsx',
+		dest: './dist/js'
+	},
+	less: {
+		src: './src/css/*',
+		dest: './dist/css'
+	},
+	static: {
+		src: './src/static/*',
+		dest: './dist/static'
+	}
+};
+
+
+gulp.task('web-pages', function() {
+	return gulp.src(paths.webpages.src, {base: './src'})
+		.pipe(changed(paths.webpages.dest))
+		.pipe(gulp.dest(paths.webpages.dest));
+});
+
+gulp.task('Less', function() {
+	return gulp.src(paths.less.src)
+		.pipe(changed(paths.less.dest))
+		.pipe(less())
+		.pipe(gulp.dest(paths.less.dest));
+});
+
+gulp.task('browserify', function() {
+	gulp.src('src/js/main.js')
+		.pipe(browserify({
+			insertGlobals : true,
+			debug : !gulp.env.production
+		}))
+		.pipe(gulp.dest('dist/js'))
+	;
+});
+
+gulp.task('default', ['web-pages', 'Less', 'browserify']);
